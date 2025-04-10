99023eea84050e2ac1b1692374dbd4178f35cbde
Sihe-Avon <puppykaya@foxmail.com>
2025-04-11 00:08:47 +0800

    Add files via upload

---
+++ src/server/get-emojis.ts
@@ -0,0 +1,24 @@
+import { Prisma } from "@prisma/client"
+import "server-only"
+import { prisma } from "./db"
+import { VALID_EMOJI_FILTER } from "./utils"
+
+export const getEmojis = async (opts: {
+  take?: number
+  skip?: number
+  orderBy?:
+    | Prisma.EmojiOrderByWithRelationInput
+    | Prisma.EmojiOrderByWithRelationInput[]
+}) => {
+  const take = opts.take ?? 100
+  const skip = opts.skip ?? undefined
+  const orderBy = opts.orderBy ?? { createdAt: Prisma.SortOrder.desc }
+
+  return prisma.emoji.findMany({
+    select: { id: true, updatedAt: true },
+    orderBy,
+    where: VALID_EMOJI_FILTER,
+    take,
+    skip,
+  })
+}
