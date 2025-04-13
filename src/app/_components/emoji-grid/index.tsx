ce6ebd4dfe53ee78b3c1095e77a314e00afa0901
Sihe-Avon <puppykaya@foxmail.com>
2025-04-13 23:01:19 +0800

    Add files via upload

---
+++ src/app/_components/emoji-grid/index.tsx
@@ -0,0 +1,43 @@
+import { EmojiCard } from "../emoji-card"
+import { fetchEmojis } from "@/server/actions"
+
+interface EmojiGridProps {
+  prompt?: string
+}
+
+interface Emoji {
+  id: string
+  prompt: string
+  originalUrl: string
+  noBackgroundUrl?: string
+  createdAt: Date
+}
+
+export async function EmojiGrid({ prompt }: EmojiGridProps) {
+  const emojis = await fetchEmojis({
+    take: 100,
+    orderBy: prompt
+      ? {
+          createdAt: "desc", // 使用创建时间排序代替全文搜索排序
+        }
+      : undefined,
+  })
+
+  return (
+    <div className="animate-in fade-in slide-in-from-bottom-4 duration-1200 ease-in-out">
+      <h2 className="font-semibold text-md text-left w-full mb-3">{!!prompt ? "Related Emojis" : "Recent Emojis"}</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 justify-items-stretch w-full">
+        {emojis.map((emoji: Emoji) => (
+          <EmojiCard 
+            key={emoji.id}
+            id={emoji.id}
+            prompt={emoji.prompt}
+            originalUrl={emoji.originalUrl}
+            noBackgroundUrl={emoji.noBackgroundUrl}
+            createdAt={emoji.createdAt}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
